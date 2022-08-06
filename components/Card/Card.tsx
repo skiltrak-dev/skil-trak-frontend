36fa6c6d1fa3c4da251f18e95bf2001489c97906
akashahmad <engr.akashahmad@gmail.com>
2022-08-06 18:52:17 +0500

    Card Component Added

---
+++ components/Card/Card.tsx
@@ -0,0 +1,16 @@
+interface CardProps {
+	children: any;
+	noPadding: boolean;
+}
+
+export const Card = ({ children, noPadding }: CardProps) => {
+	return (
+		<div
+			className={`w-full bg-white py-6 shadow-1 rounded-lg ${
+				noPadding ? "p-0" : "p-8"
+			}`}
+		>
+			{children}
+		</div>
+	);
+};
