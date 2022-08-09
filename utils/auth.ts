c5ee5c5d5fafdc492ff70430502680ccfc4c5aad
thezeeshantariq <thezeeshantariq@gmail.com>
2022-08-09 07:00:47 +0500

    Auth Utils Added

---
+++ utils/auth.ts
@@ -0,0 +1,35 @@
+import jwt from "jwt-decode";
+
+const KEYS = {
+	TOKEN: "user-token",
+};
+export const setToken = (token: string) => {
+	localStorage.setItem(KEYS.TOKEN, token);
+};
+
+export const getToken = () => {
+	return localStorage.getItem(KEYS.TOKEN);
+};
+
+export const getUserCredentials = () => {
+	const token = getToken();
+	if (token) {
+		return jwt(token);
+	}
+};
+
+export const isAuthenticated = () => {
+	return getToken() !== null;
+};
+
+export const logout = () => {
+	localStorage.removeItem(KEYS.TOKEN);
+};
+
+export const AuthUtils = {
+	setToken,
+	getToken,
+	getUserCredentials,
+	isAuthenticated,
+	logout,
+};
