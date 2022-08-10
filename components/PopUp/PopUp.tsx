57c16bba9203f91572a9783d771146a6d76ab47a
thezeeshantariq <thezeeshantariq@gmail.com>
2022-08-10 10:49:28 +0500

    Pop Up & Pagination Added

--- components/PopUp/PopUp.tsx
+++ components/PopUp/PopUp.tsx
@@ -11,11 +11,17 @@ const VariantOptions = ["success", "info", "error"] as const;
 interface PopUpProps {
 	title: string;
 	subtitle: string;
-	shadow: boolean;
+	shadow?: boolean;
 	variant: typeof VariantOptions[number];
 }
 
 export const Popup = ({ title, subtitle, shadow, variant }: PopUpProps) => {
+	const titleColor = {
+		success: "text-success",
+		info: "text-info",
+		error: "text-error",
+	};
+
 	return (
 		<div
 			className={`w-full h-60 rounded-2xl flex flex-col justify-center items-center gap-y-4 bg-white ${
@@ -23,10 +29,10 @@ export const Popup = ({ title, subtitle, shadow, variant }: PopUpProps) => {
 			}`}
 		>
 			<div>
-				<LoadingAnimation />
+				<LoadingAnimation size={80}/>
 			</div>
 			<div className="">
-				<Typography variant={"h4"} color={`text-${variant}`} center>
+				<Typography variant={"h4"} color={titleColor[variant]} center>
 					{title}
 				</Typography>
 				<Typography variant={"muted"} center color={"text-gray-500"}>
