59690423034c27f51a6a61e07577b98bc63f9f7f
thezeeshantariq <thezeeshantariq@gmail.com>
2022-08-24 10:05:02 +0500

    PDF Viewer Added

---
+++ components/PdfViewer/PdfViewer.tsx
@@ -0,0 +1,31 @@
+import { isBrowser } from "@utils";
+import { throttle } from "lodash";
+import { useEffect, useRef, useState } from "react";
+import { PdfWrapper } from "./PdfWrapper";
+
+export const PdfViewer = ({ file }: { file: string }) => {
+	const [width, setWidth] = useState<number | undefined>(undefined);
+	const wrapperRef = useRef<any>();
+
+	const setPageSize = () => {
+		setWidth(wrapperRef?.current?.getBoundingClientRect().width);
+	};
+
+	useEffect(() => {
+		if (isBrowser()) {
+			setPageSize();
+			window.addEventListener("resize", throttle(setPageSize, 500));
+
+			return window.removeEventListener(
+				"resize",
+				throttle(setPageSize, 500)
+			);
+		}
+	}, []);
+
+	return (
+		<div ref={wrapperRef}>
+			<PdfWrapper file={file} width={width} />
+		</div>
+	);
+};
---
+++ components/PdfViewer/PdfWrapper.tsx
@@ -0,0 +1,34 @@
+// ------------------------
+import { LoadingSpinner } from "@components/InputLoading";
+import { useState } from "react";
+// import { Document, Page } from "react-pdf";
+import { Document, Page, pdfjs } from "react-pdf";
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
+export const PdfWrapper = ({
+	file,
+	width,
+}: {
+	file: string;
+	width?: number;
+}) => {
+	const [totalPages, setTotalPages] = useState(null);
+	const [pageNumber, setPageNumber] = useState(1);
+
+	function onDocumentLoadSuccess({ numPages }: { numPages: any }) {
+		setTotalPages(numPages);
+	}
+
+	return (
+		<div>
+			<Document
+				file={file}
+				onLoadSuccess={onDocumentLoadSuccess}
+				loading={<LoadingSpinner loading />}
+			>
+				<Page pageNumber={pageNumber} {...(width ? { width } : {})} />
+			</Document>
+		</div>
+	);
+};
