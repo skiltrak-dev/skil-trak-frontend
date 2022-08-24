204997e5f120824550c28d42137553a70592075e
thezeeshantariq <thezeeshantariq@gmail.com>
2022-08-24 10:00:14 +0500

    Formik Form Controls Removed

--- components/formControls/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./TextInput";
-export * from "./CheckBox";
-export * from "./RadioButton";
-export * from "./Select";
-export * from "./Switch";
-export * from "./FileUpload";
-export * from "./TextArea";
