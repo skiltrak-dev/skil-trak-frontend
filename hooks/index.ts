bfdae6ad692854c7fcc5012ed9efbead3ec8c4c8
thezeeshantariq <thezeeshantariq@gmail.com>
2022-10-06 15:53:25 +0500

    Yup Resolver Export Added

--- hooks/index.ts
+++ hooks/index.ts
@@ -1,4 +1,5 @@
 export * from "./useAlert";
 export * from "./useNavbar";
 export * from "./useNotification";
-export * from "./useContextBar"
+export * from "./useContextBar";
+export * from "./useYupValidation";
