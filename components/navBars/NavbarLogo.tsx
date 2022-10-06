87fa77bfb4d27c2d8fab1fc9647a1c65a3c414b6
thezeeshantariq <thezeeshantariq@gmail.com>
2022-10-06 17:05:45 +0500

    Logo Link Fixed

--- components/navBars/NavbarLogo.tsx
+++ components/navBars/NavbarLogo.tsx
@@ -1,16 +1,18 @@
-import Link from "next/link";
-import Image from "next/image";
+import Link from 'next/link'
+import Image from 'next/image'
 
 export const HeaderLogo = () => {
-	return (
-		<Link href="/">
-			<Image
-				className="w-10/12 h-16 mx-auto"
-				src={`/images/skiltrak_logo.svg`}
-				alt="Logo"
-				height={40}
-				width={140}
-			/>
-		</Link>
-	);
-};
+    return (
+        <Link href="/">
+            <a>
+                <Image
+                    className="w-10/12 h-16 mx-auto"
+                    src={`/images/skiltrak_logo.svg`}
+                    alt="Logo"
+                    height={40}
+                    width={140}
+                />
+            </a>
+        </Link>
+    )
+}
