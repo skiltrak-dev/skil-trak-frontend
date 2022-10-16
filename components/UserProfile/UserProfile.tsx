5d36ebaa1ab0a9165420e7e21f774e1bed8ab711
thezeeshantariq <thezeeshantariq@gmail.com>
2022-10-16 08:15:38 +0500

    User Profile Added

---
+++ components/UserProfile/UserProfile.tsx
@@ -0,0 +1,123 @@
+import Image from 'next/image'
+import { FaAddressCard, FaBirthdayCake, FaUserCircle } from 'react-icons/fa'
+import { IoLocation } from 'react-icons/io5'
+import {
+    MdPhone,
+    MdBatchPrediction,
+    MdGroup,
+    MdGroups,
+    MdVerified,
+} from 'react-icons/md'
+
+export const UserProfile = () => {
+    return (
+        <div>
+            <div className="flex flex-col items-center">
+                <div className="relative">
+                    <Image
+                        src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1160&q=80"
+                        width={100}
+                        height={100}
+                        className="rounded-full shadow-inner-image"
+                    />
+                    <div className="absolute top-0 w-[100px] h-[100px] bg-transparent rounded-full shadow-inner-image"></div>
+                </div>
+
+                <div className="flex flex-col items-center">
+                    <p className="text-lg font-semibold">Name of User</p>
+                    <div className="flex items-center gap-x-2">
+                        <p className="text-sm text-gray-400">
+                            dummymail@domain.com
+                        </p>
+                        <span className="text-blue-500">
+                            <MdVerified />
+                        </span>
+                    </div>
+                </div>
+            </div>
+
+            {/* Info Row 1 */}
+            <div className="flex justify-between divide-x border-b mt-4">
+                <div className="p-2">
+                    <div className="flex items-center space-x-2">
+                        <span className="text-gray-300">
+                            <FaAddressCard />
+                        </span>
+                        <p className="text-sm font-medium">16029</p>
+                    </div>
+                    <div className="text-gray-400 text-[11px] -mt-0.5 text-center">
+                        Student ID
+                    </div>
+                </div>
+
+                <div className="p-2">
+                    <div className="flex items-center space-x-2">
+                        <span className="text-gray-300">
+                            <MdBatchPrediction />
+                        </span>
+                        <p className="text-sm font-medium">April 22</p>
+                    </div>
+                    <div className="text-gray-400 text-[11px] -mt-0.5 text-center">
+                        Batch
+                    </div>
+                </div>
+
+                <div className="p-2">
+                    <div className="flex items-center space-x-2">
+                        <span className="text-gray-300">
+                            <MdPhone />
+                        </span>
+                        <p className="text-sm font-medium">043 6456 076</p>
+                    </div>
+                    <div className="text-gray-400 text-[11px] -mt-0.5 text-center">
+                        Phone Number
+                    </div>
+                </div>
+            </div>
+
+            {/* Info Row 2 */}
+            <div className="flex justify-around divide-x border-b">
+                <div className="p-2">
+                    <div className="flex items-center space-x-2">
+                        <span className="text-gray-300">
+                            <FaBirthdayCake />
+                        </span>
+                        <p className="text-sm font-medium">11 April, 1985</p>
+                    </div>
+                    <div className="text-gray-400 text-[11px] -mt-0.5 text-center">
+                        Date Of Birth
+                    </div>
+                </div>
+
+                <div className="p-2">
+                    <div className="flex items-center space-x-2">
+                        <span className="text-gray-300">
+                            <FaUserCircle />
+                        </span>
+                        <p className="text-sm font-medium">Male</p>
+                    </div>
+                    <div className="text-gray-400 text-[11px] -mt-0.5 text-center">
+                        Gender
+                    </div>
+                </div>
+            </div>
+
+            {/* Info Row 3 */}
+            <div className="flex justify-around divide-x border-b">
+                <div className="p-2">
+                    <div className="flex items-center space-x-2">
+                        <span className="text-gray-300">
+                            <IoLocation />
+                        </span>
+                        <p className="text-sm font-medium">
+                            221B Baker Street, Sydney, Australia
+                        </p>
+                    </div>
+                    <div className="text-gray-400 text-[11px] -mt-0.5 text-center">
+                        Address
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
