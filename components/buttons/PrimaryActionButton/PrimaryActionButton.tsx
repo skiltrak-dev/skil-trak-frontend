46b62ef3e8eccc6842a0fa428830bf701bc6683f
thezeeshantariq <thezeeshantariq@gmail.com>
2022-10-16 08:12:29 +0500

    Primary Action Button Added

--- components/buttons/PrimaryActionButton/PrimaryActionButton.tsx
+++ components/buttons/PrimaryActionButton/PrimaryActionButton.tsx
@@ -1,48 +1,44 @@
-import { LottieAnimation } from "@components/LottieAnimation";
-import Link from "next/link";
-
+import { LottieAnimation } from '@components/LottieAnimation'
+import Link from 'next/link'
 
 // components
-import { Typography } from "../../Typography";
 
 export interface PrimaryActionButtonProps {
     title: string
-	description: string
-	image?: string
-	shadow?: boolean,
-	link: string,
-	animation: any
+    description: string
+    image?: string
+    shadow?: boolean
+    link: string
+    animation: any
 }
 
 export const PrimaryActionButton = ({
-	title,
-	description,
-	image,
-	shadow = true,
-	link,
-	animation,
+    title,
+    description,
+    image,
+    shadow = true,
+    link,
+    animation,
 }: PrimaryActionButtonProps) => {
-
-	return (
-		<Link
-			href={`${link}` || "/under-construction"}
-			className={`border flex justify-between items-center gap-x-5 py-2 px-4 rounded-lg w-full cursor-pointer bg-white hover:bg-gray-100`}
-		>
-			<div className="flex-grow">
-				<Typography variant={"label"}>{title}</Typography>
-				<Typography variant={"small"} color={"grayLight"}>
-					{description}
-				</Typography>
-			</div>
-			{image ? (
-				<img className="h-16" src={image} alt="Info" />
-			) : animation ? (
-				<div>
-					<LottieAnimation animation={animation} height={80} />
-				</div>
-			) : (
-				<></>
-			)}
-		</Link>
-	);
-};
+    return (
+        <Link href={`${link}` || '/under-construction'}>
+            <a
+                className={`border flex justify-between items-center gap-x-5 py-1 px-4 rounded-lg w-full cursor-pointer bg-white hover:bg-gray-50`}
+            >
+                <div className="flex-grow">
+                    <p className="text-sm font-semibold">{title}</p>
+                    <p className="text-gray-400 text-xs">{description}</p>
+                </div>
+                {image ? (
+                    <img className="h-16" src={image} alt="Info" />
+                ) : animation ? (
+                    <div>
+                        <LottieAnimation animation={animation} height={80} />
+                    </div>
+                ) : (
+                    <></>
+                )}
+            </a>
+        </Link>
+    )
+}
