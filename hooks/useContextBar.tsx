7bd7b51da5c67796e9c92a668d9e99a9da1754a5
thezeeshantariq <thezeeshantariq@gmail.com>
2022-10-16 08:15:48 +0500

    Context Bar Hook Updated

--- hooks/useContextBar.tsx
+++ hooks/useContextBar.tsx
@@ -1,39 +1,42 @@
-import React, { ReactElement, ReactNode, useContext, useState } from "react";
+import React, { ReactElement, ReactNode, useContext, useState } from 'react'
 
 interface ContextBarContextType {
-	content: ReactElement | ReactNode | undefined;
-	setContent: Function;
-	show: Function;
-	hide: Function;
-	isVisible: boolean;
+    content: ReactElement | ReactNode | undefined
+    setContent: Function
+    show: Function
+    hide: Function
+    isVisible: boolean
+    fixed: boolean
 }
 
 export const ContextBarContext =
-	React.createContext<ContextBarContextType | null>(null);
+    React.createContext<ContextBarContextType | null>(null)
 
 export const ContextBarProvider = ({
-	children,
+    children,
 }: {
-	children: ReactElement | ReactNode;
+    children: ReactElement | ReactNode
 }) => {
-	const [isVisible, setVisible] = useState(true);
-	const [content, setContent] = useState(null);
+    const [isVisible, setVisible] = useState(true)
+    const [fixed, setFixed] = useState(false)
+    const [content, setContent] = useState(null)
 
-	const value = {
-		content,
-		setContent,
-		show: () => setVisible(true),
-		hide: () => setVisible(false),
-		isVisible,
-	};
+    const value = {
+        content,
+        setContent,
+        show: () => setVisible(true),
+        hide: () => setVisible(false),
+        isVisible,
+        fixed,
+    }
 
-	return (
-		<ContextBarContext.Provider value={value}>
-			{children}
-		</ContextBarContext.Provider>
-	);
-};
+    return (
+        <ContextBarContext.Provider value={value}>
+            {children}
+        </ContextBarContext.Provider>
+    )
+}
 
 export const useContextBar = () => {
-	return useContext(ContextBarContext) as ContextBarContextType;
-};
+    return useContext(ContextBarContext) as ContextBarContextType
+}
