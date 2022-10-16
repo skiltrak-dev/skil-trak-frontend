61a0e7387020f1b4705d41dca6548ee0616198e7
thezeeshantariq <thezeeshantariq@gmail.com>
2022-10-16 08:10:40 +0500

    Next Configs Updated for Image Domains

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+    reactStrictMode: true,
+    swcMinify: true,
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: '**.unsplash.com',
+            },
+        ],
+        domains: ['images.unsplash.com', 'loremflickr.com'],
+    },
 }
 
 module.exports = nextConfig
