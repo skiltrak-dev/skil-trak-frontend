9bd87d443b995bf4e0990b082c01f27bb0364a88
imzaraf <imafaraz18@gmail.com>
2022-11-16 09:17:49 +0500

    Student Assessment Tools

--- pages/portals/student/assessments/assessment-tools/index.tsx
+++ pages/portals/student/assessments/assessment-tools/index.tsx
@@ -1,14 +1,24 @@
-import { ReactElement } from 'react'
+import { ReactElement, useEffect } from 'react'
 
 import { StudentLayout } from '@layouts'
 import { NextPageWithLayout } from '@types'
-import { AssessmentsTools } from '@components'
+import { AssessmentsTools, Button, SidebarCalendar } from '@components'
 import { Typography } from '@components'
 import { FaEdit } from 'react-icons/fa'
+import { useContextBar } from '@hooks'
 
 type Props = {}
 
 const AssessmentTools: NextPageWithLayout = (props: Props) => {
+    const { setContent } = useContextBar()
+    useEffect(() => {
+        setContent(
+            <>
+                <Button variant={'dark'} text={'My Schedule'} />
+                <SidebarCalendar />
+            </>
+        )
+    }, [setContent])
     const actions = () => {
         return (
             <div className="flex gap-x-2 ">
