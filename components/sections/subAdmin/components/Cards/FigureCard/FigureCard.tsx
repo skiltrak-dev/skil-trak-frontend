9606d466b99f54ae644420f1f49080be84f6a544
Zeeshan Tariq <thezeeshantariq@gmail.com>
2022-11-25 14:42:06 +0500

    Figure Card Updated

--- components/sections/subAdmin/components/Cards/FigureCard/FigureCard.tsx
+++ components/sections/subAdmin/components/Cards/FigureCard/FigureCard.tsx
@@ -1,36 +1,53 @@
-//components
-import { Card } from '@components/cards'
-import { Typography } from '@components'
-// image
 import Image from 'next/image'
+import CountUp from 'react-countup'
+
+import { Card } from '@components/cards'
+import { PuffLoader } from 'react-spinners'
 
 type FigureCardProps = {
-   imageUrl?: string | undefined
-   count: number
-   title: string
+    imageUrl?: string | undefined
+    count: number
+    title: string
+    loading?: boolean
 }
 
-export const FigureCard = ({ imageUrl, count, title }: FigureCardProps) => {
-   return (
-      <>
-         <Card>
-            <div className="flex justify-between">
-               {imageUrl && (
-                  <div className="flex items-center gap-x-2 justify-between">
-                     <Image
-                        src={imageUrl || ''}
-                        alt={title}
-                        width={48}
-                        height={48}
-                     />
-                  </div>
-               )}
-               <div className="flex flex-col items-end">
-                  <p className="text-3xl font-bold">{count}</p>
-                  <p className="text-xs text-gray-500 leading-3 uppercase">{title}</p>
-               </div>
-            </div>
-         </Card>
-      </>
-   )
+export const FigureCard = ({
+    imageUrl,
+    count,
+    title,
+    loading,
+}: FigureCardProps) => {
+    return (
+        <>
+            <Card>
+                <div className="flex justify-between">
+                    {imageUrl && (
+                        <div className="flex items-center gap-x-2 justify-between">
+                            <Image
+                                src={imageUrl || ''}
+                                alt={title}
+                                width={48}
+                                height={48}
+                            />
+                        </div>
+                    )}
+
+                    <div className="flex flex-col items-end">
+                        {loading ? (
+                            <div className='h-[36px]'>
+                              <PuffLoader size={28}/>
+                            </div>
+                        ) : (
+                            <p className="text-3xl font-bold">
+                                <CountUp end={count} />
+                            </p>
+                        )}
+                        <p className="text-xs text-gray-500 leading-3 uppercase">
+                            {title}
+                        </p>
+                    </div>
+                </div>
+            </Card>
+        </>
+    )
 }
