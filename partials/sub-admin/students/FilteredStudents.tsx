f4cba092c797658f8c990650903abf696ceb7948
saadkhan8573 <77046939+saadkhan8573@users.noreply.github.com>
2022-12-15 08:58:27 +0500

    Student Filter Working

---
+++ partials/sub-admin/students/FilteredStudents.tsx
@@ -0,0 +1,206 @@
+import {
+    ActionButton,
+    Card,
+    EmptyData,
+    LoadingAnimation,
+    Table,
+    TableAction,
+    TableActionOption,
+} from '@components'
+import { PageHeading } from '@components/headings'
+import { ColumnDef } from '@tanstack/react-table'
+import { Student } from '@types'
+import { useRouter } from 'next/router'
+import { ReactElement, useState } from 'react'
+import { FaEdit, FaEye } from 'react-icons/fa'
+import { MdBlock } from 'react-icons/md'
+import { StudentCellInfo } from './components'
+import { AssignStudentModal } from './modals'
+
+export const FilteredStudents = ({
+    student,
+    setPage,
+    itemPerPage,
+    setItemPerPage,
+}: {
+    student: any
+    setPage: any
+    itemPerPage: any
+    setItemPerPage: any
+}) => {
+    const router = useRouter()
+    const [modal, setModal] = useState<ReactElement | null>(null)
+
+    const onModalCancelClicked = () => {
+        setModal(null)
+    }
+    const onAssignStudentClicked = (student: Student) => {
+        setModal(
+            <AssignStudentModal
+                student={student}
+                onCancel={() => onModalCancelClicked()}
+            />
+        )
+    }
+
+    const tableActionOptions: TableActionOption[] = [
+        {
+            text: 'View',
+            onClick: (student: Student) => {
+                router.push(
+                    `/portals/sub-admin/users/students/${student.id}?tab=overview`
+                )
+            },
+            Icon: FaEye,
+        },
+        {
+            text: 'Assign to me',
+            onClick: (student: Student) => onAssignStudentClicked(student),
+            Icon: MdBlock,
+            color: 'text-red-500 hover:bg-red-100 hover:border-red-200',
+        },
+    ]
+
+    const columns: ColumnDef<any>[] = [
+        {
+            header: () => 'Name',
+            accessorKey: 'user',
+            cell: ({ row }: any) => {
+                return <StudentCellInfo student={row.original} />
+            },
+        },
+        {
+            header: () => 'Phone #',
+            accessorKey: 'phone',
+            cell: ({ row }: any) => {
+                const { phone } = row.original
+                return <p className="text-sm">{phone}</p>
+            },
+        },
+
+        {
+            header: () => 'Address',
+            accessorKey: 'address',
+            cell: ({ row }: any) => {
+                const { state, suburb } = row.original
+                return (
+                    <p className="text-sm">
+                        {suburb}, {state}
+                    </p>
+                )
+            },
+        },
+        {
+            header: () => 'RTO Name',
+            accessorKey: 'rto',
+            cell({ row }: any) {
+                const { rto } = row.original
+
+                return (
+                    <div className="flex gap-x-2 items-center">
+                        <InitialAvatar name={rto.user.name} small />
+                        {rto.user.name}
+                    </div>
+                )
+            },
+        },
+        {
+            header: () => 'Progress',
+            accessorKey: 'progress',
+            cell: ({ row }: any) => {
+                return (
+                    <div className="flex justify-center">
+                        <PlacementTableCell request={row.original.workplace} />
+                    </div>
+                )
+            },
+        },
+        {
+            header: () => 'Action',
+            accessorKey: 'Action',
+            cell: ({ row }: any) => {
+                return (
+                    <TableAction
+                        options={tableActionOptions}
+                        rowItem={row.original}
+                    />
+                )
+            },
+        },
+    ]
+
+    const quickActionsElements = {
+        id: 'id',
+        individual: (id: number) => (
+            <div className="flex gap-x-2">
+                <ActionButton Icon={FaEdit}>Edit</ActionButton>
+                <ActionButton>Sub Admins</ActionButton>
+                <ActionButton Icon={MdBlock} variant="error">
+                    Block
+                </ActionButton>
+            </div>
+        ),
+        common: (ids: number[]) => (
+            <ActionButton Icon={MdBlock} variant="error">
+                Block
+            </ActionButton>
+        ),
+    }
+
+    return (
+        <>
+            {modal && modal}
+            <div className="flex flex-col gap-y-4 p-4">
+                <PageHeading
+                    title={'Filtered Students'}
+                    subtitle={'List of Filtered Students'}
+                />
+
+                <Card noPadding>
+                    {student?.isLoading ? (
+                        <LoadingAnimation height="h-[60vh]" />
+                    ) : student?.data && student?.data?.data.length ? (
+                        <Table
+                            columns={columns}
+                            data={student?.data.data}
+                            quickActions={quickActionsElements}
+                            enableRowSelection
+                        >
+                            {({
+                                table,
+                                pagination,
+                                pageSize,
+                                quickActions,
+                            }: any) => {
+                                return (
+                                    <div>
+                                        <div className="p-6 mb-2 flex justify-between">
+                                            {pageSize(
+                                                itemPerPage,
+                                                setItemPerPage
+                                            )}
+                                            <div className="flex gap-x-2">
+                                                {quickActions}
+                                                {pagination(
+                                                    student?.data?.pagination,
+                                                    setPage
+                                                )}
+                                            </div>
+                                        </div>
+                                        <div className="px-6">{table}</div>
+                                    </div>
+                                )
+                            }}
+                        </Table>
+                    ) : (
+                        <EmptyData
+                            title={'No Students in your Search!'}
+                            description={'No Students in your Search yet'}
+                            height={'50vh'}
+                        />
+                    )}
+                </Card>
+            </div>
+        </>
+    )
+}
