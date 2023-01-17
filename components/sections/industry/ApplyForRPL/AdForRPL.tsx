d938093bdd5b01289c83f3e87a2568027313a1af
Saad@#12345 <saad.h4896@gmail.com>
2023-01-17 06:50:00 +0500

    Build Fixing

--- components/sections/industry/ApplyForRPL/AdForRPL.tsx
+++ components/sections/industry/ApplyForRPL/AdForRPL.tsx
@@ -5,52 +5,60 @@ import { useRouter } from 'next/router'
 import { Typography, Button, Card } from 'components'
 
 export const AdForRPL = ({ short }: any) => {
-  const router = useRouter()
-  return (
-    <div
-      className={`flex ${short ? 'flex-col' : 'flex-col lg:flex-row'
-        } items-center gap-x-8`}
-    >
-      {short ? (
-        <img
-          className="h-36 rounded-lg w-full object-cover"
-          src="/images/content/rpl.jpg"
-          alt="Other"
-        />
-      ) : (
-        <img
-          className="h-full rounded-lg"
-          src="/images/content/rpl.jpg"
-          alt="Other"
-          width={180}
-          height={180}
-        />
-      )}
+    const router = useRouter()
+    return (
+        <div
+            className={`flex ${
+                short ? 'flex-col' : 'flex-col lg:flex-row'
+            } items-center gap-x-8`}
+        >
+            {short ? (
+                <img
+                    className="h-36 rounded-lg w-full object-cover"
+                    src="/images/content/rpl.jpg"
+                    alt="Other"
+                />
+            ) : (
+                <img
+                    className="h-full rounded-lg"
+                    src="/images/content/rpl.jpg"
+                    alt="Other"
+                    width={180}
+                    height={180}
+                />
+            )}
 
-      <div className={`w-full h-full flex flex-col items-start gap-y-5`}>
-        {!short && (
-          <div className="flex flex-col gap-y-2.5">
-            <h2 className='text-2xl font-bold'>RPL - Become Qualified Fast.</h2>
-            <p className="text-gray-500">
-              Use your work experience to achieve a nationally recognized qualification.
-            </p>
+            <div className={`w-full h-full flex flex-col items-start gap-y-5`}>
+                {!short && (
+                    <div className="flex flex-col gap-y-2.5">
+                        <h2 className="text-2xl font-bold">
+                            RPL - Become Qualified Fast.
+                        </h2>
+                        <p className="text-gray-500">
+                            Use your work experience to achieve a nationally
+                            recognized qualification.
+                        </p>
 
-            <p className="text-gray-500">
-              Get your experience recognized and the qualification you deserve. All qualification you are awarded by our partnered RTO's.
-            </p>
-          </div>
-        )}
+                        <p className="text-gray-500">
+                            Get your experience recognized and the qualification
+                            you deserve. All qualification you are awarded by
+                            our partnered RTO&apos;s.
+                        </p>
+                    </div>
+                )}
 
-        {/*  */}
-        <div className={`${short && 'mt-2'}`}>
-          <Button
-            variant={'primary'}
-            onClick={() => router.push('/portals/industry/tasks/apply-for-rpl')}
-          >
-            Apply For RPL
-          </Button>
+                {/*  */}
+                <div className={`${short && 'mt-2'}`}>
+                    <Button
+                        variant={'primary'}
+                        onClick={() =>
+                            router.push('/portals/industry/tasks/apply-for-rpl')
+                        }
+                    >
+                        Apply For RPL
+                    </Button>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  )
+    )
 }
