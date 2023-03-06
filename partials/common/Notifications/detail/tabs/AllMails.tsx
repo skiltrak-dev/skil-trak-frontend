5a53a6e871e2d7f96dc40fb202fce16e49306d74
imzaraf <imafaraz18@gmail.com>
2023-03-06 15:57:22 +0500

    Notifications Pagination

--- partials/common/Notifications/detail/tabs/AllMails.tsx
+++ partials/common/Notifications/detail/tabs/AllMails.tsx
@@ -76,17 +76,17 @@ export const AllMails = ({
             >
                 {allMails && allMails?.length > 0
                     ? allMails?.map((message: any) => (
-                          <MailListCard
-                              key={message?.id}
-                              message={message}
-                              selectedMessageId={selectedMessage?.id}
-                              onClick={() => {
-                                  setSelectedMessage(message)
-                              }}
-                          />
-                      ))
+                        <MailListCard
+                            key={message?.id}
+                            message={message}
+                            selectedMessageId={selectedMessage?.id}
+                            onClick={() => {
+                                setSelectedMessage(message)
+                            }}
+                        />
+                    ))
                     : !isError &&
-                      isSuccess && <NoData text={'There is no mails'} />}
+                    isSuccess && <NoData text={'There is no mails'} />}
                 {isError && (
                     <NoData
                         text={
