9b1419114c70f6e5eba6b405f4793532c9574ead
saadkhan8573 <saad.h4896@gmail.com>
2023-06-09 18:16:15 +0500

    View Industry Checks

--- partials/industry/currentStudents/contextBar/ViewFoldersCB.tsx
+++ partials/industry/currentStudents/contextBar/ViewFoldersCB.tsx
@@ -1,19 +1,88 @@
 import { useContextBar } from '@hooks'
-import { useEffect, Fragment } from 'react'
-import { Typography, ActionButton, NoData } from '@components'
+import { useEffect, Fragment, useState, ReactElement } from 'react'
+import {
+    Typography,
+    ActionButton,
+    NoData,
+    PdfViewModal,
+    FileViewModal,
+    VideoPlayModal,
+} from '@components'
 import { ellipsisText } from '@utils'
 import { AiFillEye } from 'react-icons/ai'
 import { FaCloudDownloadAlt } from 'react-icons/fa'
 
 export const ViewFoldersCB = ({ folders }: any) => {
+    const [modal, setModal] = useState<ReactElement | null>(null)
     const { setTitle } = useContextBar()
 
     useEffect(() => {
         setTitle('View Folders')
     }, [])
 
+    const onModalCancel = () => {
+        setModal(null)
+    }
+
+    const getImageViewModal = (file: any) => {
+        return (
+            <FileViewModal
+                title=""
+                subtitle=""
+                url={file?.file}
+                onCancelButtonClick={onModalCancel}
+            >
+                <div className="max-w-[650px] relative">
+                    <img src={file?.file} alt="" className="max-w-full" />
+                </div>
+            </FileViewModal>
+        )
+    }
+
+    const onFileClicked = (file: any) => {
+        // setSelected(file)
+
+        if (
+            ['jpg', 'jpeg', 'png', 'jfif'].includes(
+                file?.extension?.toLowerCase()
+            )
+        ) {
+            setModal(getImageViewModal(file))
+        } else if (
+            ['pdf', 'document'].includes(file?.extension?.toLowerCase())
+        ) {
+            // const fileSplit = file.file.split('https://')
+            // const url = `https://www.${fileSplit[1]}`
+            const url = `${file?.file}`
+            setModal(
+                <PdfViewModal
+                    url={url}
+                    downloadUrl={file?.file}
+                    onCancelButtonClick={onModalCancel}
+                    extension={file?.extension}
+                />
+            )
+        } else if (
+            ['mp4', 'mkv', 'avi', 'mpeg', 'quicktime', 'mov'].includes(
+                file?.extension?.toLowerCase()
+            )
+        ) {
+            // const fileSplit = file.file.split('https://')
+            // const url = `https://www.${fileSplit[1]}`
+            setModal(
+                <VideoPlayModal
+                    // url={url}
+                    url={file?.file}
+                    downloadUrl={file?.file}
+                    onCancelButtonClick={onModalCancel}
+                />
+            )
+        }
+    }
+
     return (
         <>
+            {modal}
             <div>
                 {folders && folders?.length > 0 ? (
                     folders?.map((folder: any) => (
@@ -46,10 +115,65 @@ export const ViewFoldersCB = ({ folders }: any) => {
                                                     simple
                                                     Icon={AiFillEye}
                                                     variant="success"
+                                                    onClick={() => {
+                                                        let fileName = uploaded
+                                                            ? uploaded?.file?.split(
+                                                                  '\\'
+                                                              )
+                                                            : ''
+                                                        if (
+                                                            fileName?.length ===
+                                                            1
+                                                        ) {
+                                                            fileName =
+                                                                uploaded?.file?.split(
+                                                                    '/'
+                                                                )
+
+                                                            if (
+                                                                fileName.length >
+                                                                1
+                                                            ) {
+                                                                fileName =
+                                                                    fileName[
+                                                                        fileName?.length -
+                                                                            1
+                                                                    ]
+                                                            }
+                                                        }
+                                                        const extension =
+                                                            fileName
+                                                                ?.replaceAll(
+                                                                    '{"',
+                                                                    ''
+                                                                )
+                                                                .replaceAll(
+                                                                    '"}',
+                                                                    ''
+                                                                )
+                                                                ?.split('.')
+                                                                .reverse()[0]
+                                                        onFileClicked({
+                                                            ...uploaded,
+                                                            file: uploaded?.file
+                                                                .replaceAll(
+                                                                    '{"',
+                                                                    ''
+                                                                )
+                                                                .replaceAll(
+                                                                    '"}',
+                                                                    ''
+                                                                ),
+                                                            extension,
+                                                            type:
+                                                                uploaded?.type ||
+                                                                'all',
+                                                        })
+                                                    }}
                                                 >
-                                                    <a href={uploaded?.file}>
-                                                        View
-                                                    </a>
+                                                    {/* <a href={uploaded?.file}> */}
+                                                    View
+                                                    {/* </a> */}
                                                 </ActionButton>
                                                 <ActionButton
                                                     simple
