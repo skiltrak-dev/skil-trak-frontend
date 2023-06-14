26c26379f7ad5ebc90149e7803262e8d121686f1
saadkhan8573 <saad.h4896@gmail.com>
2023-06-14 15:55:34 +0500

    Student Call History

--- partials/common/History/HistoryCard.tsx
+++ partials/common/History/HistoryCard.tsx
@@ -1,13 +1,47 @@
 import { Timeline, Typography, UserCreatedAt } from '@components'
+import moment from 'moment'
 import React from 'react'
+import { ImPhone, ImPhoneHangUp } from 'react-icons/im'
 
-export const HistoryCard = ({ history }: { history: any }) => {
+export const HistoryCard = ({
+    history,
+    call,
+}: {
+    history: any
+    call?: boolean
+}) => {
+    const today = moment()
+    const startDate = today.startOf('week').format('MM-DD-YYYY')
+    const endDate = today.endOf('week').format('MM-DD-YYYY')
+    const createdAt = moment(history?.createdAt, 'YYYY-MM-DD')
+
+    const isDateExist = createdAt.isBetween(startDate, endDate, 'day')
     return (
         <Timeline key={history?.id} updatedAt={history?.updatedAt}>
             <div className="grid grid-cols-3 items-center bg-white rounded-md px-3 py-1 w-full">
                 <Typography>
                     <span className="text-[11px] block">Task</span>
-                    {history?.title}
+                    <div className="flex items-center gap-x-2">
+                        {history?.title}
+
+                        {call &&
+                            isDateExist &&
+                            (history.isAnswered ? (
+                                <div className="rounded-full bg-success p-0.5">
+                                    <ImPhone
+                                        title={'Call Made and Answered'}
+                                        className="text-white text-[10px]"
+                                    />
+                                </div>
+                            ) : history.isAnswered === false ? (
+                                <div className="rounded-full bg-red-700 p-0.5">
+                                    <ImPhoneHangUp
+                                        title={'Call Made and Not Answered'}
+                                        className="text-white text-[10px]"
+                                    />
+                                </div>
+                            ) : null)}
+                    </div>
                 </Typography>
                 <Typography>
                     <span className="text-[11px] block">Description</span>
