00b51bfee94f5399efb78c36436e1f97307fd4d5
Zeeshan Tariq <thezeeshantariq@gmail.com>
2023-09-14 11:22:50 +0500

    Filter name changed

--- components/index.ts
+++ components/index.ts
@@ -76,7 +76,7 @@ export * from './Badge'
 export * from './InitialAvatar'
 
 export * from './TabNavigation'
-export * from './Filters'
+export * from './filtersx'
 export * from './Table'
 export * from './cards'
 
