305b747fe0a69cf685e7bccecb26f06dc6cb0447
imzaraf <imafaraz18@gmail.com>
2024-02-19 11:07:43 +0500

    Blog Short Description Issue Fixed on Mobile

--- partials/common/Blogs/components/BlogCard.tsx
+++ partials/common/Blogs/components/BlogCard.tsx
@@ -1,3 +1,5 @@
+import { useWindowWidth } from '@hooks'
+import { ellipsisText } from '@utils'
 import moment from 'moment'
 import Image from 'next/image'
 import Link from 'next/link'
@@ -14,6 +16,11 @@ export const BlogCard = ({
     shortDescription,
 }: any) => {
     const router = useRouter()
+    function isMobile(width: any) {
+        return width <= 768
+    }
+    const width = useWindowWidth()
+    const mobile = isMobile(width)
     return (
         <div className="bg-[#FFFCF7] rounded-xl shadow-md px-2 py-1.5 h-[390px]">
             <Link href={`blogs/${slug}`}>
@@ -37,7 +44,7 @@ export const BlogCard = ({
                         </p>
                     </div>
 
-                    <h1 className="font-bold uppercase mb-1.5">{title}</h1>
+                    <h1 className="font-bold uppercase mb-1.5">{ellipsisText(title, 26)}</h1>
                     <div
                         className="blog-content block mr-6 text-gray-400 text-xs"
                         // dangerouslySetInnerHTML={{
@@ -45,7 +52,7 @@ export const BlogCard = ({
                         // }}
                     >
                         {(shortDescription &&
-                            shortDescription?.substr(0, 460) + '...') ||
+                            shortDescription?.substr(0, mobile ? 180 : 460) + '...') ||
                             ' '}
                     </div>
                 </div>
