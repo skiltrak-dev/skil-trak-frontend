31d87801dfe4f33d2d9319d506afc5bda72c5956
saadkhan8573 <saad.h4896@gmail.com>
2024-02-20 13:01:23 +0500

    Student Assessments Results

---
+++ partials/common/StudentProfileDetail/components/AssessmentsSubmission/components/AssessmentFiles/AddComment.tsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import { SubmitFolderCommentForm } from '../../forms'
+import { useAddCommentOnAssessmentMutation } from '@queries'
+import { AddCommentEnum } from '@types'
+import { ShowErrorNotifications } from '@components'
+import { useNotification } from '@hooks'
+
+export const AddComment = ({
+    comment,
+    resultId,
+    studentId,
+    assessmentFolder,
+    assessmentResponseId,
+    folderStatus,
+}: {
+    comment: string
+    resultId: number
+    studentId: number
+    assessmentFolder: any
+    assessmentResponseId: number
+    folderStatus: AddCommentEnum
+}) => {
+    const { notification } = useNotification()
+
+    const [addComment, addCommentResult] = useAddCommentOnAssessmentMutation()
+
+    const onSubmit = (values: any) => {
+        addComment({
+            resultId,
+            std: Number(studentId),
+            comment: values.comment,
+            folderId: assessmentResponseId,
+            status: values?.type as AddCommentEnum,
+            assessmentFolderId: assessmentFolder?.id,
+        }).then((res: any) => {
+            if (res?.data) {
+                notification.success({
+                    title: 'Comment Added',
+                    description: 'Comment Added Successfully',
+                })
+            }
+        })
+    }
+    return (
+        <div>
+            <ShowErrorNotifications result={addCommentResult} />
+            <SubmitFolderCommentForm
+                comment={comment}
+                onSubmit={onSubmit}
+                result={addCommentResult}
+                folderStatus={folderStatus}
+                assessmentFolder={assessmentFolder}
+            />
+        </div>
+    )
+}
---
+++ partials/common/StudentProfileDetail/components/AssessmentsSubmission/components/SubmitAssessmentSubmission.tsx
@@ -0,0 +1,84 @@
+import { Button } from '@components'
+import { Result } from '@constants'
+import { useNotification } from '@hooks'
+import { useSubmitStudentAssessmentMutation } from '@queries'
+import { Student } from '@types'
+import { getCourseResult } from '@utils'
+import React, { useEffect } from 'react'
+
+export const SubmitAssessmentSubmission = ({
+    selectedCourseId,
+    student,
+    isFilesUploaded,
+    results,
+    isResubmittedFiles,
+}: {
+    selectedCourseId: number
+    student: Student | undefined
+    isFilesUploaded: boolean | undefined
+    results: any
+    isResubmittedFiles?: boolean
+}) => {
+    const { notification } = useNotification()
+    const [submitAssessment, submitAssessmentResult] =
+        useSubmitStudentAssessmentMutation()
+
+    useEffect(() => {
+        if (submitAssessmentResult.isSuccess) {
+            notification.success({
+                title: 'Assessment Submitted Successfully',
+                description: 'Assessment Submitted Successfully',
+            })
+        }
+    }, [submitAssessmentResult])
+
+    const result = getCourseResult(results)
+
+    const onSubmitAssessment = () => {
+        submitAssessment({
+            body: {
+                notifyCoordinator: false,
+                notifyRto: false,
+            },
+            student: student?.user?.id,
+            id: selectedCourseId,
+        })
+    }
+
+    useEffect(() => {
+        if (
+            isFilesUploaded &&
+            !results?.length &&
+            result?.result === Result.NotSubmitted &&
+            !submitAssessmentResult.isLoading
+        ) {
+            onSubmitAssessment()
+        }
+    }, [isFilesUploaded, submitAssessmentResult, results, result])
+
+    useEffect(() => {
+        if (
+            isResubmittedFiles &&
+            results?.length > 0 &&
+            result?.status !== Result.Pending &&
+            !submitAssessmentResult.isLoading
+        ) {
+            onSubmitAssessment()
+        }
+    }, [result, results, isResubmittedFiles, submitAssessmentResult])
+
+    const onSubmit = (values: any) => {
+        onSubmitAssessment()
+    }
+    return (
+        <div>
+            <Button
+                text="Submit Assessment"
+                onClick={onSubmit}
+                variant="info"
+                loading={submitAssessmentResult.isLoading}
+                disabled={submitAssessmentResult.isLoading}
+            />
+        </div>
+    )
+}
