6f0a3e2efb19507d50451249a06d3b5cdff9b040
saadkhan8573 <saad.h4896@gmail.com>
2024-03-11 13:58:28 +0500

    Traineship Program

--- partials/frontPages/OurServices/UpskillTraineeship/components/ContactUpSkill.tsx
+++ partials/frontPages/OurServices/UpskillTraineeship/components/ContactUpSkill.tsx
@@ -1,13 +1,45 @@
 import React from 'react'
 import { UpskillForm } from '../form'
+import { CommonApi } from '@queries'
+import { useNotification } from '@hooks'
+import { ShowErrorNotifications } from '@components'
+import { getAge } from '@utils'
+import moment from 'moment'
 
 export const ContactUpSkill = () => {
+    const [addTraineeship, addTraineeshipResult] =
+        CommonApi.Traineeship.useAddTraineeship()
+
+    const { notification } = useNotification()
+
     const onSubmit = (values: any) => {
-        console.log({ values })
+        var dob = moment(values?.dob)
+
+        // Get the current date using moment
+        var currentDate = moment()
+
+        // Calculate the age using moment
+        var age = Number(currentDate.diff(dob, 'years'))
+        console.log({ age })
+
+        addTraineeship({ ...values, age })?.then((res: any) => {
+            if (res?.data) {
+                notification.success({
+                    title: 'Traineeship Added',
+                    description: 'Traineeship Added Successfuly',
+                })
+            }
+        })
     }
     return (
-        <div className="rounded-[10px] shadow-site px-6 py-5 bg-white">
-            <UpskillForm onSubmit={onSubmit} />
-        </div>
+        <>
+            <ShowErrorNotifications result={addTraineeshipResult} />
+            <div className="rounded-[10px] shadow-site px-6 py-5 bg-white">
+                <UpskillForm
+                    onSubmit={onSubmit}
+                    result={addTraineeshipResult}
+                />
+            </div>
+        </>
     )
 }
