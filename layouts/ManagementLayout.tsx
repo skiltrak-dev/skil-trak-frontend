f96937978abf55ddc92e0f682c12110e1c850651
imzaraf <imafaraz18@gmail.com>
2024-04-30 10:28:20 +0500

    Protected Routes Added

--- layouts/ManagementLayout.tsx
+++ layouts/ManagementLayout.tsx
@@ -8,16 +8,16 @@ import { ReactNode, useState } from 'react'
 export const ManagementLayout = ({ children }: { children: ReactNode }) => {
     const [isExpanded, setIsExpanded] = useState<boolean>(false)
     return (
-        // <ProtectedRoute>
-        <div className="management-portal-log h-screen flex flex-col gap-y-4 overflow-hidden pb-8 px-6 pt-6 w-full">
-            <DisplayNotifications />
-            <ManagementNavbar
-                setIsExpanded={setIsExpanded}
-                isExpanded={isExpanded}
-            />
+        <ProtectedRoute>
+            <div className="management-portal-log h-screen flex flex-col gap-y-4 overflow-hidden pb-8 px-6 pt-6 w-full">
+                <DisplayNotifications />
+                <ManagementNavbar
+                    setIsExpanded={setIsExpanded}
+                    isExpanded={isExpanded}
+                />
 
-            {children}
-        </div>
-        // </ProtectedRoute>
+                {children}
+            </div>
+        </ProtectedRoute>
     )
 }
