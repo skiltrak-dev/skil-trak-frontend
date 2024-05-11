d56c91401929856a7d1533a38a12e6aa98aa2317
saadkhan8573 <saad.h4896@gmail.com>
2024-05-11 11:28:37 +0500

    Error Boundries Added

--- layouts/SiteLayout.tsx
+++ layouts/SiteLayout.tsx
@@ -1,4 +1,4 @@
-import { DisplayNotifications } from '@components'
+import { DisplayNotifications, ErrorBoundary } from '@components'
 import { Footer4 } from '@components/site'
 import { Navbar2 } from '@components/site/navbar/Navbar2'
 import TawkMessengerReact from '@tawk.to/tawk-messenger-react'
@@ -17,7 +17,9 @@ export const SiteLayout = ({ children, title }: any) => {
             <Navbar2 />
             <DisplayNotifications />
             <main>{children}</main>
-            <Footer4 />
+            <ErrorBoundary>
+                <Footer4 />
+            </ErrorBoundary>
             <TawkMessengerReact
                 propertyId={'61b1f52ec82c976b71c091e2'}
                 widgetId={'1fmfibg61'}
