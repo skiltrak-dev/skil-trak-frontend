5663e6738f3df8ccb44ea9ad4585e8e5a9b0d318
saadkhan8573 <saad.h4896@gmail.com>
2024-05-12 15:50:12 +0500

    Footer Setting

--- layouts/SiteLayout.tsx
+++ layouts/SiteLayout.tsx
@@ -1,8 +1,8 @@
-import { useRef } from 'react'
+import { DisplayNotifications } from '@components'
 import { Footer4 } from '@components/site'
 import { Navbar2 } from '@components/site/navbar/Navbar2'
 import TawkMessengerReact from '@tawk.to/tawk-messenger-react'
-import { DisplayNotifications, ErrorBoundary } from '@components'
+import { useRef } from 'react'
 
 export const SiteLayout = ({ children, title }: any) => {
     const tawkMessengerRef = useRef<any>()
@@ -17,14 +17,14 @@ export const SiteLayout = ({ children, title }: any) => {
             <Navbar2 />
             <DisplayNotifications />
             <main>{children}</main>
-            <ErrorBoundary>
-                <Footer4 />
-            </ErrorBoundary>
-            <TawkMessengerReact
-                propertyId={'61b1f52ec82c976b71c091e2'}
-                widgetId={'1fmfibg61'}
-                useRef={tawkMessengerRef}
-            />
+            <Footer4 />
+            <div>
+                <TawkMessengerReact
+                    propertyId={'61b1f52ec82c976b71c091e2'}
+                    widgetId={'1fmfibg61'}
+                    useRef={tawkMessengerRef}
+                />
+            </div>
         </div>
     )
 }
