b8f21c3703b0ad913412142f02aa664d90338641
saadkhan8573 <edottossm@gmail.com>
2024-06-11 13:59:29 +0500

    Change Industry Profile Detail

--- pages/portals/admin/industry/[id]/detail.tsx
+++ pages/portals/admin/industry/[id]/detail.tsx
@@ -1,24 +1,360 @@
-import { EmptyData, LoadingAnimation, TechnicalError } from '@components'
+import {
+    ActionButton,
+    Alert,
+    AuthorizedUserComponent,
+    BackButton,
+    Button,
+    EmptyData,
+    IndustryProfile,
+    LoadingAnimation,
+    TechnicalError,
+} from '@components'
+import { useActionModal, useAlert, useContextBar, useNavbar } from '@hooks'
 import { AdminLayout } from '@layouts'
-import { IndustryProfileDetail } from '@partials/common'
-import { AdminApi } from '@queries'
-import { NextPageWithLayout } from '@types'
+import { Industry, NextPageWithLayout, UserStatus } from '@types'
 import { useRouter } from 'next/router'
-import React, { ReactElement } from 'react'
+import { ReactElement, ReactNode, useEffect, useState } from 'react'
+import { FaArchive, FaBan, FaEdit } from 'react-icons/fa'
 
-const IndustryDetail: NextPageWithLayout = () => {
+import { DetailTabs } from '@partials/admin/industry/tabs'
+import { AdminApi } from '@queries'
+
+import { FigureCard } from '@components/sections/subAdmin'
+import { PinnedNotes } from '@partials'
+import { useActionModals } from '@partials/admin/industry/hooks/useActionModals'
+import { AcceptModal } from '@partials/admin/industry/modals'
+import { SnoozeIndustryModal, UnSnoozeIndustryModal } from '@partials/common'
+import { getUserCredentials } from '@utils'
+import moment from 'moment'
+import { MdSnooze } from 'react-icons/md'
+import { UserRoles } from '@constants'
+
+const Detail: NextPageWithLayout = () => {
+    const [newModal, setNewModal] = useState<ReactNode | null>(null)
+    const [isMouseMove, setIsMouseMove] = useState<any>(null)
     const router = useRouter()
+    const navBar = useNavbar()
+    const contextBar = useContextBar()
+
+    const { alert, setAlerts } = useAlert()
+
+    useEffect(() => {
+        navBar.setTitle('Industry Detail')
+    }, [])
+
+    useEffect(() => {
+        window.addEventListener('mousemove', () => setIsMouseMove(true))
+        return () => {
+            window.removeEventListener('mousemove', () => setIsMouseMove(false))
+        }
+    }, [])
+
+    useEffect(() => {
+        if (!contextBar.content) {
+            setIsMouseMove(false)
+        }
+    }, [contextBar])
+
+    const {
+        modal,
+        // onAcceptClicked,
+        onRejectClicked,
+        onArchiveClicked,
+        onUnblockClicked,
+        onDeleteClicked,
+        onUnArchiveClicked,
+        onBlockClicked,
+    } = useActionModals()
+
+    const onAcceptClicked = (industry: any) => {
+        setNewModal(
+            <AcceptModal
+                industry={industry}
+                onCancel={() => setNewModal(null)}
+            />
+        )
+    }
+
     const industry = AdminApi.Industries.useDetail(Number(router.query.id), {
         skip: !router.query?.id,
         refetchOnMountOrArgChange: true,
     })
+    const industryStatisticsCount = AdminApi.Industries.useStatisticsCount(
+        Number(industry?.data?.user?.id),
+        { skip: !industry?.data?.user?.id }
+    )
+
+    useEffect(() => {
+        navBar.setSubTitle('Industry Detail')
+        navBar.setSubTitle(industry.data?.user?.name)
+    }, [industry.data])
+    useEffect(() => {
+        if (industry.isSuccess) {
+            if (industry?.data) {
+                contextBar.setContent(<IndustryProfile data={industry.data} />)
+                contextBar.show(false)
+            }
+            const showAlert = () => {
+                switch (industry.data?.user?.status) {
+                    case UserStatus.Pending:
+                        alert.warning({
+                            title: 'Industry is Pending',
+                            description: 'Industry is Pending',
+                            autoDismiss: false,
+                        })
+                        break
+                    case UserStatus.Archived:
+                        alert.warning({
+                            title: 'Industry is Archived',
+                            description: 'Industry is Archived',
+                            autoDismiss: false,
+                        })
+                        break
+                    case UserStatus.Rejected:
+                        alert.error({
+                            title: 'Industry is Rejected',
+                            description: 'Industry is Rejected',
+                            autoDismiss: false,
+                        })
+                        break
+                    case UserStatus.Blocked:
+                        alert.error({
+                            title: 'Industry is Blocked',
+                            description: 'Industry is Blocked',
+                            autoDismiss: false,
+                        })
+                        break
+
+                    default:
+                        break
+                }
+            }
+            showAlert()
+        }
+
+        return () => {
+            setAlerts([])
+            contextBar.setContent(null)
+            contextBar.hide()
+        }
+    }, [industry, isMouseMove])
+
+    const role = getUserCredentials()?.role
+
+    const { passwordModal, onViewPassword } = useActionModal()
+
+    const statusBaseActions = () => {
+        switch (industry.data?.user?.status) {
+            case UserStatus.Pending:
+                return (
+                    <div className="flex items-center gap-x-2">
+                        <ActionButton
+                            variant={'success'}
+                            Icon={FaArchive}
+                            onClick={() => onAcceptClicked(industry?.data)}
+                        >
+                            Accept
+                        </ActionButton>
+                        <ActionButton
+                            Icon={FaBan}
+                            variant={'error'}
+                            onClick={() => onRejectClicked(industry?.data)}
+                        >
+                            Reject
+                        </ActionButton>
+                    </div>
+                )
+            case UserStatus.Approved:
+                return (
+                    <div className="flex gap-x-2">
+                        <ActionButton
+                            Icon={FaEdit}
+                            onClick={() => {
+                                router.push(
+                                    role === 'admin'
+                                        ? `/portals/admin/industry/edit-industry/${router.query.id}`
+                                        : `/portals/sub-admin/users/industries/${router.query.id}/edit-profile`
+                                )
+                            }}
+                        >
+                            Edit
+                        </ActionButton>
+                        <ActionButton
+                            Icon={FaArchive}
+                            onClick={() => onArchiveClicked(industry?.data)}
+                        >
+                            Archive
+                        </ActionButton>
+                        <ActionButton
+                            Icon={FaBan}
+                            variant={'error'}
+                            onClick={() => onBlockClicked(industry?.data)}
+                        >
+                            Block
+                        </ActionButton>
+                    </div>
+                )
+            case UserStatus.Blocked:
+                return (
+                    <div className="flex items-center gap-x-2">
+                        <ActionButton
+                            Icon={FaArchive}
+                            onClick={() => onUnblockClicked(industry?.data)}
+                        >
+                            Un Block
+                        </ActionButton>
+                        <ActionButton
+                            Icon={FaBan}
+                            variant={'error'}
+                            onClick={() => onDeleteClicked(industry?.data)}
+                        >
+                            Delete
+                        </ActionButton>
+                    </div>
+                )
+            case UserStatus.Rejected:
+                return (
+                    <div className="flex items-center gap-x-2">
+                        <ActionButton
+                            Icon={FaArchive}
+                            onClick={() => onAcceptClicked(industry?.data)}
+                        >
+                            Accept
+                        </ActionButton>
+                        <ActionButton
+                            Icon={FaBan}
+                            variant={'error'}
+                            onClick={() => onDeleteClicked(industry?.data)}
+                        >
+                            Delete
+                        </ActionButton>
+                    </div>
+                )
+            case UserStatus.Archived:
+                return (
+                    <div className="flex items-center gap-x-2">
+                        <ActionButton
+                            Icon={FaArchive}
+                            onClick={() => onUnArchiveClicked(industry?.data)}
+                        >
+                            Un Archive
+                        </ActionButton>
+                        <ActionButton
+                            Icon={FaBan}
+                            variant={'error'}
+                            onClick={() => onDeleteClicked(industry?.data)}
+                        >
+                            Delete
+                        </ActionButton>
+                    </div>
+                )
+
+            default:
+                return
+        }
+    }
+
+    const onCancelModal = () => setNewModal(null)
+
+    const onSnooze = () => {
+        setNewModal(
+            <SnoozeIndustryModal
+                onCancel={onCancelModal}
+                industry={industry?.data as Industry}
+            />
+        )
+    }
+
+    const UnSnoozeModal = () => {
+        setNewModal(
+            <UnSnoozeIndustryModal
+                onCancel={onCancelModal}
+                industry={industry?.data as Industry}
+            />
+        )
+    }
+
     return (
-        <div>
+        <>
+            {newModal}
+            {passwordModal}
             {industry.isError && <TechnicalError />}
             {industry.isLoading || industry?.isFetching ? (
                 <LoadingAnimation height={'h-[70vh]'} />
             ) : industry.data ? (
-                <IndustryProfileDetail industry={industry?.data} />
+                <div className="p-6 flex flex-col gap-y-4">
+                    {industry?.data?.snoozedDate && (
+                        <Alert
+                            title="Industry Snoozed"
+                            description={`Industry Snoozed till ${moment(
+                                industry?.data?.snoozedDate
+                            ).format('MMM DD YYYY')}`}
+                            variant="warning"
+                            autoDismiss={false}
+                        />
+                    )}
+                    {modal && modal}
+                    {/* Action Bar */}
+                    <div className="flex items-center justify-between">
+                        <BackButton text="Industries" />
+                        <div className="flex items-center gap-x-2">
+                            <AuthorizedUserComponent roles={[UserRoles.ADMIN]}>
+                                <Button
+                                    text={'View Password'}
+                                    onClick={() => {
+                                        onViewPassword({
+                                            user: industry?.data?.user,
+                                        })
+                                    }}
+                                />
+                            </AuthorizedUserComponent>
+
+                            <Button
+                                text="Book Appointment"
+                                variant="info"
+                                onClick={() => {
+                                    router.push({
+                                        pathname:
+                                            '/portals/admin/appointment-type/create-appointment',
+                                        query: {
+                                            industry: industry?.data?.user?.id,
+                                        },
+                                    })
+                                }}
+                                disabled={!industry.isSuccess}
+                            />
+                            <Button
+                                text={
+                                    industry?.data?.snoozedDate
+                                        ? 'Un Snooze'
+                                        : 'Snooze'
+                                }
+                                variant="action"
+                                Icon={MdSnooze}
+                                onClick={() => {
+                                    industry?.data?.snoozedDate
+                                        ? UnSnoozeModal()
+                                        : onSnooze()
+                                }}
+                            />
+                            {statusBaseActions()}
+                        </div>
+                    </div>
+                    <div className="flex">
+                        <FigureCard
+                            imageUrl="/images/icons/students.png"
+                            count={Number(industryStatisticsCount?.data?.count)}
+                            title={'Current Students'}
+                            link={`/portals/admin/industry/${industry?.data?.id}?tab=students`}
+                        />
+                    </div>
+
+                    <PinnedNotes
+                        id={industry?.data?.user?.id}
+                        link={`/portals/admin/industry/${router.query.id}?tab=notes`}
+                    />
+                    <DetailTabs id={router.query.id} industry={industry} />
+                </div>
             ) : (
                 !industry.isError &&
                 industry.isSuccess && (
@@ -28,12 +364,12 @@ const IndustryDetail: NextPageWithLayout = () => {
                     />
                 )
             )}
-        </div>
+        </>
     )
 }
 
-IndustryDetail.getLayout = (page: ReactElement) => {
+Detail.getLayout = (page: ReactElement) => {
     return <AdminLayout>{page}</AdminLayout>
 }
 
-export default IndustryDetail
+export default Detail
