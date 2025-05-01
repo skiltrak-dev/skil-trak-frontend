13fd9eef999b3f022c049c9f155e9931615251a0
saadkhan8573 <saad.h4896@gmail.com>
2025-05-01 10:57:29 +0500

    Build Error Fix

--- partials/common/Notifications/NotificationList.tsx
+++ partials/common/Notifications/NotificationList.tsx
@@ -1,38 +1,20 @@
 import { useState } from 'react'
-// Layouts
-// Types
-import { Typography } from '@components'
-import {
-    AllNotifications,
-    ReadNotifications,
-    UnReadNotifications,
-} from '@partials/common'
-import { IoIosNotificationsOutline } from 'react-icons/io'
 
-const tabs = [
-    {
-        text: 'All Notifications',
-        component: AllNotifications,
-    },
-    {
-        text: 'Unread Notifications',
-        component: UnReadNotifications,
-    },
-    {
-        text: 'Read Notifications',
-        component: ReadNotifications,
-    },
-]
+import { Typography } from '@components'
+import { IoIosNotificationsOutline } from 'react-icons/io'
+import { notificationTabs } from './data'
 
 export const NotificationList = () => {
-    const [selectedTab, setSelectedTab] = useState(tabs?.[0]?.text)
+    const [selectedTab, setSelectedTab] = useState(notificationTabs?.[0]?.text)
 
-    const Component = tabs?.find((tab) => tab?.text === selectedTab)?.component
+    const Component = notificationTabs?.find(
+        (tab) => tab?.text === selectedTab
+    )?.component
 
     return (
         <div className="flex flex-col gap-y-2">
             <div className="bg-white px-6 pt-3 flex items-center gap-x-14">
-                {tabs?.map((tab) => (
+                {notificationTabs?.map((tab) => (
                     <div
                         className={`cursor-pointer pb-3 flex items-center gap-x-2 border-b-4 border-white ${
                             tab?.text === selectedTab ? '!border-primary' : ''
