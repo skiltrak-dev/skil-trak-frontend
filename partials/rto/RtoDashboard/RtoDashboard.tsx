860a5c724a0f5d2bc10385071220208bdd54a010
Ahmad Faraz <imafaraz18@gmail.com>
2025-06-25 07:55:40 +0500

    RTO Course based chart

--- partials/rto/RtoDashboard/RtoDashboard.tsx
+++ partials/rto/RtoDashboard/RtoDashboard.tsx
@@ -27,7 +27,10 @@ export const RtoDashboard = ({ rto }: { rto: Rto }) => {
     return (
         <div className="flex flex-col gap-y-8 mb-12">
             <div>
-                <RtoDashboardStatistics rto={rto} />
+                <RtoDashboardStatistics
+                    rto={rto}
+                    rtoUserId={getUserCredentials()?.id}
+                />
             </div>
             <div>
                 <RtoDashboardStudents />
