7fb24e570de51b9051341cb394c739d647e1da8a
saadkhan8573 <saad.h4896@gmail.com>
2025-08-06 10:43:55 +0500

    New Hello

--- pages/portals/sub-admin/todo-list-details.tsx
+++ pages/portals/sub-admin/todo-list-details.tsx
@@ -1,12 +1,19 @@
-import { ReactElement } from 'react'
+import { ReactElement, useEffect } from 'react'
 import { NextPageWithLayout } from '@types'
 
 // layouts
 import { SubAdminLayout } from '@layouts'
 import { TodoTabs } from '@partials/common/todoList'
+import { useRouter } from 'next/router'
 // import { TodoTabs } from '@partials/common'
 
 const TodoListDetails: NextPageWithLayout = () => {
+    const router = useRouter()
+
+    useEffect(() => {
+        router.push('/portals/sub-admin')
+    }, [])
+
     return (
         <div className="flex flex-col gap-y-2">
             <TodoTabs />
