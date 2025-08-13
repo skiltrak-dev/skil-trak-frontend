bb241282ff06a5df6a35984d34402ebcafc6c87c
saadkhan8573 <saad.h4896@gmail.com>
2025-08-13 08:00:33 +0500

    asdasdas

--- partials/common/StudentProfileDetail/components/AllCommunication/components/VirtualizedCommunicationList.tsx
+++ partials/common/StudentProfileDetail/components/AllCommunication/components/VirtualizedCommunicationList.tsx
@@ -5,7 +5,7 @@ export const VirtualizedCommunicationList: React.FC<VirtualizedListProps> = ({
     items,
 }) => (
     <>
-        {items.map((item) => (
+        {items?.map((item) => (
             <CollapsedCommunicationCard key={item.id} item={item} />
         ))}
     </>
