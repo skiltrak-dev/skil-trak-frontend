12aad1d6847e6ad893b1255c5ff7d8d798e7cd1d
saadkhan8573 <saad.h4896@gmail.com>
2025-09-02 08:27:04 +0500

    asd

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@
 const nextConfig = {
     // reactStrictMode: true,
     env: {
-        googleDirectionApi: 'AIzaSyApOsp5NyUUJyW3vlZtvQ4IYf8urG7rrKA',
+        googleDirectionApi: 'AIzaSyB3Q7z-z5t-MYrBqXaqk7E24cxjaXABJdk',
         mapBoxApi:
             'pk.eyJ1Ijoic2tpbHRyYWsiLCJhIjoiY20zMm1oZG9wMTRzMTJrc2N2dHluN3ZjOCJ9.J0XKK9V8faX1iTWj1ED3Kg',
         cloudflareSiteKey: '0x4AAAAAAA73xTWozjUM9dfN',
